faca576c4535902a7473a380475a20fb5b4c8855
ozakiryota <kb22056@gmail.com>
2020-10-24 14:30:49 +0900

    updated

--- pysrc/compare_rp.py
+++ pysrc/compare_rp.py
@@ -8,98 +8,88 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
+import os
+import csv
+
 class CompareRPY:
     def __init__(self):
         print("--- compare_rpy ---")
+        ## parameter-graph
+        self.erase_old_data = rospy.get_param("/erase_old_data", True)
+        print("self.erase_old_data = ", self.erase_old_data)
+        self.ini_shown_size = rospy.get_param("/ini_shown_size", 100)
+        if not self.erase_old_data:
+            self.ini_shown_size = 1
+        print("self.ini_shown_size = ", self.ini_shown_size)
+        self.fix_ylim = rospy.get_param("/fix_ylim", False)
+        print("self.fix_ylim = ", self.fix_ylim)
+        self.ylim = rospy.get_param("/ylim", 45.0)
+        print("self.ylim = ", self.ylim)
+        self.axis_resolution = rospy.get_param("/axis_resolution", 10.0)
+        print("self.axis_resolution = ", self.axis_resolution)
+        self.interval = rospy.get_param("/interval", 0.1)
+        print("self.interval = ", self.interval)
+        ## parameter-method
+        self.num_sub = rospy.get_param("/num_sub", 1)
+        print("self.num_sub = ", self.num_sub)
+        self.list_method_name = []
+        for method_idx in range(self.num_sub):
+            method_name = rospy.get_param("/method" + str(method_idx), "method" + str(method_idx))
+            self.list_method_name.append(method_name)
+        print("self.list_method_name = ", self.list_method_name)
+        ## parameter-csv
+        current_dir = os.path.dirname(os.path.abspath(__file__))
+        csv_name = "rp"
+        for method_idx in range(self.num_sub):
+            csv_name = csv_name + "_" + self.list_method_name[method_idx]
+        csv_path = rospy.get_param("/csv_path", current_dir + "/../save/" + csv_name + ".csv")
+        self.csv_file = open(csv_path, "w")
+        print("csv_path = ", csv_path)
         ## subscriber
-        self.sub_truth = rospy.Subscriber("/truth/rpy", Vector3Stamped, self.callbackTruth, queue_size=1)
-        self.sub_estimation = rospy.Subscriber("/estimation/rpy", Vector3Stamped, self.callbackEstimation, queue_size=1)
-        ## publisher
-        self.pub_error = rospy.Publisher("/error/rpy", Vector3Stamped, queue_size=1)
+        self.list_sub = []
+        for method_idx in range(self.num_sub):
+            sub_estimation = rospy.Subscriber("/estimation" + str(method_idx) + "/rpy", Vector3Stamped, self.callbackEstimation, callback_args=method_idx, queue_size=1)
+            self.list_sub.append(sub_estimation)
         ## msg
-        self.truth = Vector3Stamped()
-        self.estimation = Vector3Stamped()
-        self.error = Vector3Stamped()
+        self.list_estimation_msg = []
+        for _ in range(self.num_sub):
+            self.list_estimation_msg.append(Vector3Stamped())
         ## list
         self.list_t = []
-        self.list_truth_r = []
-        self.list_truth_p = []
-        self.list_estimation_r = []
-        self.list_estimation_p = []
-        self.list_error_rp = []
+        self.list_list_estimation_r = []
+        self.list_list_estimation_p = []
+        for _ in range(self.num_sub):
+            self.list_list_estimation_r.append([])
+            self.list_list_estimation_p.append([])
         ## line
-        self.line_truth_r = None
-        self.line_truth_p = None
-        self.line_estimation_r = None
-        self.line_estimation_p = None
+        self.list_line_estimation_r = []
+        self.list_line_estimation_p = []
         ## time
         self.start_time = time.time()
         ## flag
-        self.got_first_truth = False
+        self.got_first_msg = False
         self.got_new_msg = False
-        ## parameter
-        self.erase_old_data = rospy.get_param("/erase_old_data", True)
-        print("self.erase_old_data = ", self.erase_old_data)
-        self.interval = 0.1
-        self.ylim = 45.0
-        self.shown_size = 100
-
         ## initialization
         self.initializePlot()
+        self.initializeCSV()
         ## loop
         self.mainLoop()
 
-    def callbackTruth(self, msg):
-        self.truth = msg
-        self.got_first_truth = True
-
-    def callbackEstimation(self, msg):
-        self.estimation = msg
-        if self.got_first_truth:
-            self.computeError()
-            self.publication()
-            self.got_new_msg = True
-    
-    def computeError(self):
-        self.error.vector.x = self.computeAngleDiff(
-            self.truth.vector.x,
-            self.estimation.vector.x
-        )
-        self.error.vector.y = self.computeAngleDiff(
-            self.truth.vector.y,
-            self.estimation.vector.y
-        )
-        self.error.vector.z = self.computeAngleDiff(
-            self.truth.vector.z,
-            self.estimation.vector.z
-        )
-        ## append
-        self.list_error_rp.append([self.error.vector.x, self.error.vector.y])
-        ## print
-        arr_error_rp = np.array(self.list_error_rp)
-        print("arr_error_rp.shape = ", arr_error_rp.shape)
-        print("MAE[deg]: ", self.computeMAE(arr_error_rp)/math.pi*180.0)
-    
-    def computeAngleDiff(self, x, y):
-        diff = math.atan2(math.sin(x - y), math.cos(x - y))
-        return diff
-
-    def computeMAE(self, x):
-        return np.mean(np.abs(x), axis=0)
-
-    def publication(self):
-        self.error.header = self.estimation.header
-        self.pub_error.publish(self.error)
+    def callbackEstimation(self, msg, method_idx):
+        self.list_estimation_msg[method_idx] = msg
+        self.got_new_msg = True
+        if not self.got_first_msg:
+            self.start_time = time.time()
+            self.got_first_msg = True
 
     def initializePlot(self):
         plt.ion()   #interactive mode on
         plt.figure()
         ## empty
-        self.list_t = [0 for i in range(self.shown_size)]
-        self.list_truth_r = [0 for i in range(self.shown_size)]
-        self.list_truth_p = [0 for i in range(self.shown_size)]
-        self.list_estimation_r = [0 for i in range(self.shown_size)]
-        self.list_estimation_p = [0 for i in range(self.shown_size)]
+        self.list_t = [0 for _ in range(self.ini_shown_size)]
+        for method_idx in range(self.num_sub):
+            self.list_list_estimation_r[method_idx] = [0 for _ in range(self.ini_shown_size)]
+            self.list_list_estimation_p[method_idx] = [0 for _ in range(self.ini_shown_size)]
         ## roll
         plt.subplot(2, 1, 1)
         # plt.title("roll")
@@ -107,8 +97,9 @@ class CompareRPY:
         plt.ylabel("roll[deg]")
         plt.ylim(-self.ylim, self.ylim)
         plt.grid(True)
-        self.line_truth_r, = plt.plot(self.list_t, self.list_truth_r, label="Truth")   #get line
-        self.line_estimation_r, = plt.plot(self.list_t, self.list_estimation_r, label="Est.")   #get line
+        for method_idx in range(self.num_sub):
+            line_estimation_r, = plt.plot(self.list_t, self.list_list_estimation_r[method_idx], label=self.list_method_name[method_idx])   #get line
+            self.list_line_estimation_r.append(line_estimation_r)
         plt.legend()
         ## pitch
         plt.subplot(2, 1, 2)
@@ -117,14 +108,23 @@ class CompareRPY:
         plt.ylabel("pitch[deg]")
         plt.ylim(-self.ylim, self.ylim)
         plt.grid(True)
-        self.line_truth_p, = plt.plot(self.list_t, self.list_truth_p, label="Truth")   #get line
-        self.line_estimation_p, = plt.plot(self.list_t, self.list_estimation_p, label="Est.")   #get line
+        for method_idx in range(self.num_sub):
+            line_estimation_p, = plt.plot(self.list_t, self.list_list_estimation_p[method_idx], label=self.list_method_name[method_idx])   #get line
+            self.list_line_estimation_p.append(line_estimation_p)
         plt.legend()
+
+    def initializeCSV(self):
+        writer = csv.writer(self.csv_file)
+        row = ["time"]
+        for method_idx in range(self.num_sub):
+            row = row + [self.list_method_name[method_idx] + "_r", self.list_method_name[method_idx] + "_p"]
+        writer.writerow(row)
 
     def mainLoop(self):
         while not rospy.is_shutdown():
             if self.got_new_msg:
                 self.updatePlot()
+                self.writeCSV()
                 self.got_new_msg = False
             self.drawPlot()
 
@@ -132,33 +132,60 @@ class CompareRPY:
         ## append
         t = time.time() - self.start_time
         self.list_t.append(t)
-        self.list_truth_r.append(self.truth.vector.x/math.pi*180.0)
-        self.list_truth_p.append(self.truth.vector.y/math.pi*180.0)
-        self.list_estimation_r.append(self.estimation.vector.x/math.pi*180.0)
-        self.list_estimation_p.append(self.estimation.vector.y/math.pi*180.0)
+        for method_idx in range(self.num_sub):
+            self.list_list_estimation_r[method_idx].append(self.list_estimation_msg[method_idx].vector.x/math.pi*180.0)
+            self.list_list_estimation_p[method_idx].append(self.list_estimation_msg[method_idx].vector.y/math.pi*180.0)
         ## pop
         if self.erase_old_data:
             self.list_t.pop(0)
-            self.list_truth_r.pop(0)
-            self.list_truth_p.pop(0)
-            self.list_estimation_r.pop(0)
-            self.list_estimation_p.pop(0)
+            for method_idx in range(self.num_sub):
+                self.list_list_estimation_r[method_idx].pop(0)
+                self.list_list_estimation_p[method_idx].pop(0)
         ## roll
         plt.subplot(2,1,1)
-        self.line_truth_r.set_xdata(self.list_t)
-        self.line_truth_r.set_ydata(self.list_truth_r)
-        self.line_estimation_r.set_xdata(self.list_t)
-        self.line_estimation_r.set_ydata(self.list_estimation_r)
+        ## roll-data
+        for method_idx in range(self.num_sub):
+            self.list_line_estimation_r[method_idx].set_xdata(self.list_t)
+            self.list_line_estimation_r[method_idx].set_ydata(self.list_list_estimation_r[method_idx])
+        ## roll-axis
         plt.xlim(min(self.list_t), max(self.list_t))
-        plt.title("MAE[deg]: " + "{:.3f}".format(self.computeMAE(np.array(self.list_error_rp))[0]/math.pi*180.0))
+        if not self.fix_ylim:
+            all_r = []
+            for method_idx in range(self.num_sub):
+                all_r= all_r + self.list_list_estimation_r[method_idx]
+            plt.ylim(min(all_r), max(all_r))
+        ## roll-title
+        title_r = "NowR[deg]:"
+        for method_idx in range(self.num_sub):
+            title_r = title_r + " " + self.list_method_name[method_idx] \
+                + "{:.3f}".format(self.list_list_estimation_p[method_idx][-1])
+        plt.title(title_r)
         ## pitch
         plt.subplot(2,1,2)
-        self.line_truth_p.set_xdata(self.list_t)
-        self.line_truth_p.set_ydata(self.list_truth_p)
-        self.line_estimation_p.set_xdata(self.list_t)
-        self.line_estimation_p.set_ydata(self.list_estimation_p)
+        ## pitch-data
+        for method_idx in range(self.num_sub):
+            self.list_line_estimation_p[method_idx].set_xdata(self.list_t)
+            self.list_line_estimation_p[method_idx].set_ydata(self.list_list_estimation_p[method_idx])
+        ## pitch-axis
         plt.xlim(min(self.list_t), max(self.list_t))
-        plt.title("MAE[deg]: " + "{:.3f}".format(self.computeMAE(np.array(self.list_error_rp))[1]/math.pi*180.0))
+        if not self.fix_ylim:
+            all_p = []
+            for method_idx in range(self.num_sub):
+                all_p = all_p + self.list_list_estimation_p[method_idx]
+            plt.ylim(min(all_p), max(all_p))
+        ## pitch-title
+        title_p = "NowP[deg]:"
+        for method_idx in range(self.num_sub):
+            title_p = title_p + " " + self.list_method_name[method_idx] \
+                + "{:.3f}".format(self.list_list_estimation_p[method_idx][-1])
+        plt.title(title_p)
+
+    def writeCSV(self):
+        writer = csv.writer(self.csv_file)
+        row = [self.list_t[-1]]
+        for method_idx in range(self.num_sub):
+            row = row + [self.list_list_estimation_r[method_idx][-1], self.list_list_estimation_p[method_idx][-1]]  #rp
+        writer.writerow(row)
 
     def drawPlot(self):
         ## layout
---
+++ pysrc/compare_rp_with_gt_1method.py
@@ -0,0 +1,178 @@
+#!/usr/bin/env python
+
+import rospy
+from geometry_msgs.msg import Vector3Stamped
+
+import math
+import time
+import matplotlib.pyplot as plt
+import numpy as np
+
+class CompareRPY:
+    def __init__(self):
+        print("--- compare_rpy ---")
+        ## subscriber
+        self.sub_truth = rospy.Subscriber("/truth/rpy", Vector3Stamped, self.callbackTruth, queue_size=1)
+        self.sub_estimation = rospy.Subscriber("/estimation/rpy", Vector3Stamped, self.callbackEstimation, queue_size=1)
+        ## publisher
+        self.pub_error = rospy.Publisher("/error/rpy", Vector3Stamped, queue_size=1)
+        ## msg
+        self.truth = Vector3Stamped()
+        self.estimation = Vector3Stamped()
+        self.error = Vector3Stamped()
+        ## list
+        self.list_t = []
+        self.list_truth_r = []
+        self.list_truth_p = []
+        self.list_estimation_r = []
+        self.list_estimation_p = []
+        self.list_error_rp = []
+        ## line
+        self.line_truth_r = None
+        self.line_truth_p = None
+        self.line_estimation_r = None
+        self.line_estimation_p = None
+        ## time
+        self.start_time = time.time()
+        ## flag
+        self.got_first_truth = False
+        self.got_new_msg = False
+        ## parameter
+        self.erase_old_data = rospy.get_param("/erase_old_data", True)
+        print("self.erase_old_data = ", self.erase_old_data)
+        self.interval = 0.1
+        self.ylim = 45.0
+        self.shown_size = 100
+
+        ## initialization
+        self.initializePlot()
+        ## loop
+        self.mainLoop()
+
+    def callbackTruth(self, msg):
+        self.truth = msg
+        self.got_first_truth = True
+
+    def callbackEstimation(self, msg):
+        self.estimation = msg
+        if self.got_first_truth:
+            self.computeError()
+            self.publication()
+            self.got_new_msg = True
+    
+    def computeError(self):
+        self.error.vector.x = self.computeAngleDiff(
+            self.truth.vector.x,
+            self.estimation.vector.x
+        )
+        self.error.vector.y = self.computeAngleDiff(
+            self.truth.vector.y,
+            self.estimation.vector.y
+        )
+        self.error.vector.z = self.computeAngleDiff(
+            self.truth.vector.z,
+            self.estimation.vector.z
+        )
+        ## append
+        self.list_error_rp.append([self.error.vector.x, self.error.vector.y])
+        ## print
+        arr_error_rp = np.array(self.list_error_rp)
+        print("arr_error_rp.shape = ", arr_error_rp.shape)
+        print("MAE[deg]: ", self.computeMAE(arr_error_rp)/math.pi*180.0)
+    
+    def computeAngleDiff(self, x, y):
+        diff = math.atan2(math.sin(x - y), math.cos(x - y))
+        return diff
+
+    def computeMAE(self, x):
+        return np.mean(np.abs(x), axis=0)
+
+    def publication(self):
+        self.error.header = self.estimation.header
+        self.pub_error.publish(self.error)
+
+    def initializePlot(self):
+        plt.ion()   #interactive mode on
+        plt.figure()
+        ## empty
+        self.list_t = [0 for i in range(self.shown_size)]
+        self.list_truth_r = [0 for i in range(self.shown_size)]
+        self.list_truth_p = [0 for i in range(self.shown_size)]
+        self.list_estimation_r = [0 for i in range(self.shown_size)]
+        self.list_estimation_p = [0 for i in range(self.shown_size)]
+        ## roll
+        plt.subplot(2, 1, 1)
+        # plt.title("roll")
+        plt.xlabel("time[s]")
+        plt.ylabel("roll[deg]")
+        plt.ylim(-self.ylim, self.ylim)
+        plt.grid(True)
+        self.line_truth_r, = plt.plot(self.list_t, self.list_truth_r, label="Truth")   #get line
+        self.line_estimation_r, = plt.plot(self.list_t, self.list_estimation_r, label="Est.")   #get line
+        plt.legend()
+        ## pitch
+        plt.subplot(2, 1, 2)
+        # plt.title("pitch")
+        plt.xlabel("time[s]")
+        plt.ylabel("pitch[deg]")
+        plt.ylim(-self.ylim, self.ylim)
+        plt.grid(True)
+        self.line_truth_p, = plt.plot(self.list_t, self.list_truth_p, label="Truth")   #get line
+        self.line_estimation_p, = plt.plot(self.list_t, self.list_estimation_p, label="Est.")   #get line
+        plt.legend()
+
+    def mainLoop(self):
+        while not rospy.is_shutdown():
+            if self.got_new_msg:
+                self.updatePlot()
+                self.got_new_msg = False
+            self.drawPlot()
+
+    def updatePlot(self):
+        ## append
+        t = time.time() - self.start_time
+        self.list_t.append(t)
+        self.list_truth_r.append(self.truth.vector.x/math.pi*180.0)
+        self.list_truth_p.append(self.truth.vector.y/math.pi*180.0)
+        self.list_estimation_r.append(self.estimation.vector.x/math.pi*180.0)
+        self.list_estimation_p.append(self.estimation.vector.y/math.pi*180.0)
+        ## pop
+        if self.erase_old_data:
+            self.list_t.pop(0)
+            self.list_truth_r.pop(0)
+            self.list_truth_p.pop(0)
+            self.list_estimation_r.pop(0)
+            self.list_estimation_p.pop(0)
+        ## roll
+        plt.subplot(2,1,1)
+        self.line_truth_r.set_xdata(self.list_t)
+        self.line_truth_r.set_ydata(self.list_truth_r)
+        self.line_estimation_r.set_xdata(self.list_t)
+        self.line_estimation_r.set_ydata(self.list_estimation_r)
+        plt.xlim(min(self.list_t), max(self.list_t))
+        plt.title("MAE[deg]: " + "{:.3f}".format(self.computeMAE(np.array(self.list_error_rp))[0]/math.pi*180.0))
+        ## pitch
+        plt.subplot(2,1,2)
+        self.line_truth_p.set_xdata(self.list_t)
+        self.line_truth_p.set_ydata(self.list_truth_p)
+        self.line_estimation_p.set_xdata(self.list_t)
+        self.line_estimation_p.set_ydata(self.list_estimation_p)
+        plt.xlim(min(self.list_t), max(self.list_t))
+        plt.title("MAE[deg]: " + "{:.3f}".format(self.computeMAE(np.array(self.list_error_rp))[1]/math.pi*180.0))
+
+    def drawPlot(self):
+        ## layout
+        plt.tight_layout()
+        ## draw
+        plt.draw()
+        plt.pause(self.interval)
+
+def main():
+    rospy.init_node('compare_rpy', anonymous=True)
+
+    compare_rpy = CompareRPY()
+
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
